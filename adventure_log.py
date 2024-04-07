bde08efb26e29ce1b997c09b2ee4ee28773d6cf9
Alinka8 <alinazoldubaeva@gmail.com>
2024-04-07 01:00:16 -0500

    add initial project description

---
+++ adventure_log.py
@@ -0,0 +1,5 @@
+def greet_coding_temple():
+    print("Ahay, are you ready for adventure? ")
+
+if __name__ == "__main__":
+    greet_coding_temple()
---
+++ app.py
@@ -0,0 +1,3 @@
+numbers=range(10)
+for number in numbers:
+    print(number)
